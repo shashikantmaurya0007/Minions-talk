159b3ae3a766ba2c88ecd1f64f017eab414e74d4
shashikantmaurya0007 <80745766+shashikantmaurya0007@users.noreply.github.com>
2021-09-26 21:22:00 +0530

    completed

---
+++ app.js
@@ -0,0 +1,20 @@
+var buttontranslate = document.querySelector("#btn-translate");
+var textinput = document.querySelector("#text-input");
+var output = document.querySelector("#output-area");
+
+function errorhandler() {
+    alert("something wrong with server please try later");
+}
+
+function eventhandler() {
+    var s = textinput.value;
+    fetch("https://api.funtranslations.com/translate/" + "?" + "text=" + s)
+        .then((response) => response.json())
+        .then((json) => {
+            var translated = json.contents.translated;
+            output.innerText = translated;
+        })
+        .catch(errorhandler);
+}
+
+buttontranslate.addEventListener("click", eventhandler);
